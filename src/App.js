5595e0d0b48feedbc0a36df88b2bf9e12d9cee8f
Rohan Palankar <rohanpalankar8@gmail.com>
2023-02-23 09:26:04 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+
+import React, {useState, useRef} from 'react';
+
 import './App.css';
 
+const apiUrl = "https://jsonplaceholder.typicode.com/users";
+
 function App() {
+  console.log("Component Rendered");
+  const [userList, setUserList] = useState([]);
+  // const [count, setCount] = useState(0);
+  // const [initialUserList, setInitialUserList] = useState([]);
+  const count = useRef(0);
+  const initialUserList = useRef([]);
+
+  const getUsers = async () => {
+    let users = await fetch(apiUrl);
+    users = await users.json();
+    setUserList(users);
+    // setInitialUserList(users);
+    initialUserList.current = users;
+  }
+
+  const sortList = () => {
+    if (count.current == 0) {
+      let users = [...userList];
+      users.sort((a, b) => {
+        return a.name.length - b.name.length
+      })
+      setUserList(users);
+      // setCount(1);
+      count.current = 1;
+    }
+    else if (count.current == 1){
+      let users = [...userList];
+      users.sort((a, b) => {
+        return b.name.length - a.name.length
+      })
+      setUserList(users);
+      // setCount(2);
+      count.current = 2;
+    }
+    else if (count.current == 2){
+      setUserList(initialUserList.current);
+      // setCount(0);
+      count.current = 0;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>User List</h1>
+      <div>
+        <button onClick={getUsers}>Get Users</button>
+        <button onClick={sortList}>Sort list by name's length</button>
+      </div>
+      <ul>
+        {userList.map((user,key) => <li key={key}>{user.name}</li>)}
+      </ul>
+    </main>
   );
 }
 
